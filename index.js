bc155118aab82d1a70de80a61fb51f72f33ea2d0
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-02-13 09:59:51 -0600

    first version of markdown generator complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
   {
@@ -65,7 +66,7 @@ const questions = [
 inquirer.prompt(questions).then((data) => {
   console.log(data);
   const fileName = `${data.title}.md`;
-  console.log(fileName);
+  generateMarkdown(data);
 });
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,54 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(`# ${data.title}
+  
+  ## Badge(s)
+  
+  ## Description
 
-`;
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Credits](#Credits)
+  - [Contributing](#Contributing)
+  - [Test](#Tests)
+  - [License](License)
+  - [Questions](Questions)
+  
+  ## Installation
+
+  ${data.install}
+  
+  ## Usage
+
+  ${data.usage}
+  
+  ## Credits
+
+  ${data.credits}
+  
+  ## Contributing
+  
+  Please review our community standards for contributing, adopted from the [Contributor Covenant](https://www.contributor-covenant.org/).
+  
+  ## Tests
+
+  ${data.tests}
+  
+  ## License
+
+  ${data.license}
+  
+  ## Questions
+  
+  Questions? Please contact by [email](${data.email}. Feel free to check out additional projects at [gitHub profile name](https://github.com/${data.gitHub})
+  
+
+`);
 }
 
 module.exports = generateMarkdown;
